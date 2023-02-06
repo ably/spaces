c587130d77a00f48bd8bc8b58e1256759b4db573
Peter Maguire <peter.maguire@ably.com>
2023-02-06 12:07:28 +0000

    proof of concept websocket mock

--- src/spaces.test.ts
+++ src/spaces.test.ts
@@ -2,6 +2,7 @@ import { it, describe, expect, expectTypeOf, beforeEach } from 'vitest';
 import Ably, { Types } from 'ably/promises';
 import Space from './Space';
 import Spaces from './Spaces';
+import { Server, WebSocket } from 'mock-socket';
 
 describe('Core Space API functionality', () => {
   it('Expects the injected client to be of the type RealtimePromise', () => {
@@ -12,11 +13,37 @@ describe('Core Space API functionality', () => {
     expectTypeOf(spaces.ably).toMatchTypeOf<Types.RealtimePromise>();
   });
   it('Connects successfully with the Ably Client', async () => {
+    Ably.Realtime.Platform.Config.WebSocket = WebSocket;
+    const server = new Server('wss://realtime.ably.io/');
+    server.on('connection', (socket)=>{
+      socket.send(JSON.stringify({
+        "action": 4,
+        "connectionId": "CONNDESC",
+        "connectionKey": "CONNECTIONKEY",
+        "connectionSerial": -1,
+        "connectionDetails": {
+          "clientId": "RND-CLIENTID",
+          "connectionKey": "randomKey",
+          "maxMessageSize": 131000,
+          "maxInboundRate": 1000,
+          "maxOutboundRate": 1000,
+          "maxFrameSize": 262144,
+          "connectionStateTtl": 120000,
+          "maxIdleInterval": 15000
+        }
+      }))
+      socket.on('message', (m)=>{
+        console.log(m);
+      })
+    })
     const ablyClient = new Ably.Realtime({
       key: 'abc:def',
+      useBinaryProtocol: false,
+      log: {
+        level: 4,
+        handler: console.log,
+      }
     });
-    const notConnected = await ablyClient.connection.whenState("disconnected");
-    expect(notConnected.current).toBe('disconnected');
     const connectSuccess = await ablyClient.connection.whenState("connected");
     expect(connectSuccess.current).toBe('connected');
   });
