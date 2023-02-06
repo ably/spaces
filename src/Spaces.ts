04ab63de43a385188ab2d6727c7bccfbd2e89800
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2023-02-06 09:42:58 +0000

    Add shape for entry method

--- src/Spaces.ts
+++ src/Spaces.ts
@@ -3,12 +3,15 @@ import SpaceOptions from "./Options/SpaceOptions";
 import Space from './Space';
 class Spaces {
   private spaces: Record<string, Space>;
+  private channel: Types.RealtimeChannelPromise;
 
   constructor(
     private ably: Types.RealtimePromise
   ){
     this.spaces = {};
     this.ably = ably;
+    // The channel name prefix here should be unique to avoid conflicts with non-space channels
+    this.channel = ably.channels.get(`_ably_space_${name}`);
   }
 
   get(name: string, options: SpaceOptions): Space {
@@ -21,6 +24,18 @@ class Spaces {
     const space = new Space(name, options, this.ably);
     this.spaces[name] = space;
     return space;
+  }
+
+  enter(data: unknown) {
+    if (!data || typeof data !== 'object') {
+      return;
+    }
+
+    const clientId = this.ably.auth.clientId || undefined;
+    const presence = this.channel.presence;
+
+    // TODO: Discuss if we actually want change this behaviour in contrast to presence (enter becomes an update)
+    presence.get({ clientId }).then();
   }
 
 }
