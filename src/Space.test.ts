eca4e7652bbec9fbb2dbec423f8eeb94050686b6
Dominik Piatek <dominik.piatek@ably.io>
2023-02-07 15:46:08 +0000

    Refactor tests

--- src/Space.test.ts
+++ src/Space.test.ts
@@ -1,214 +1,80 @@
-import { it, describe, expect, expectTypeOf, vi, beforeEach } from 'vitest';
+import { it, describe, expect, expectTypeOf, vi, beforeEach, afterEach } from 'vitest';
 import Ably, { Types } from 'ably/promises';
+import { WebSocket } from 'mock-socket';
+
 import Space from './Space';
-import { Server as MockSocketServer, WebSocket } from 'mock-socket';
-
-const createMockServer = () => {
-  (Ably.Realtime as any).Platform.Config.WebSocket = WebSocket;
-  return new Server('wss://realtime.ably.io/');
-}
-
-const waitForFirstMessage = (server) => {
-  server.on('connection', (socket) => {
-    socket.send(
-      JSON.stringify({
-        action: 4,
-        connectionId: 'CONNDESC',
-        connectionKey: 'CONNECTIONKEY',
-        connectionSerial: -1,
-        connectionDetails: {
-          clientId: 'RND-CLIENTID',
-          connectionKey: 'randomKey',
-          maxMessageSize: 131000,
-          maxInboundRate: 1000,
-          maxOutboundRate: 1000,
-          maxFrameSize: 262144,
-          connectionStateTtl: 120000,
-          maxIdleInterval: 15000,
-        },
-      })
-    );
-  });
-};
-
-class Server {
-  server: MockSocketServer;
-  actions: number;
-
-  constructor() {
-    (Ably.Realtime as any).Platform.Config.WebSocket = WebSocket;
-    
-    this.server = new MockSocketServer('wss://realtime.ably.io/');
-    this.actions = 1;
-  }
-
-  connect() {
-    const actions = this.actions;
-
-    this.server.on('connection', (socket) => {
-      socket.send(
-        JSON.stringify({
-          action: 4,
-          connectionId: 'CONNDESC',
-          connectionKey: 'CONNECTIONKEY',
-          connectionSerial: -1,
-          connectionDetails: {
-            clientId: 'RND-CLIENTID',
-            connectionKey: 'randomKey',
-            maxMessageSize: 131000,
-            maxInboundRate: 1000,
-            maxOutboundRate: 1000,
-            maxFrameSize: 262144,
-            connectionStateTtl: 120000,
-            maxIdleInterval: 15000,
-          },
-        })
-      );
-
-      socket.on('message', (msg) => {
-        // const parsed = JSON.parse(msg as string);
-        console.log(actions)
-      })
-    });
-  }
-
-  addAction() {
-    this.actions = 2;
-  }
-
-  // onAction(code, callback) {
-  //   this.socket.on('message', (msg) => {
-  //     const obj = JSON.parse(msg as string);
-  //     if (obj.action === code) {
-
-  //     }
-  //   })
-
-  //   callback(message, socket);
-  // }
-}
-// server.onAction(15, (message, socket)=>{socket.send()})
+import Server from '../utilities/test/mock-server';
+import defaultClientConfig from '../utilities/test/default-client-config';
+import {
+  enterPresenceAction,
+  getPresenceAction,
+  createChannelAction,
+} from '../utilities/test/mock-server-action-responses';
 
 interface SpaceTestContext {
-  client: Types.RealtimePromise,
+  client: Types.RealtimePromise;
+  server: Server;
 }
 
-describe('Functionality of Space instances', () => {
-  beforeEach<SpaceTestContext>(async (context) => {
-    context.client = new Ably.Realtime({
-      key: 'abc:def',
+describe('Space', () => {
+  beforeEach<SpaceTestContext>((context) => {
+    (Ably.Realtime as any).Platform.Config.WebSocket = WebSocket;
+    context.server = new Server('wss://realtime.ably.io/');
+    context.client = new Ably.Realtime(defaultClientConfig);
+  });
+
+  afterEach<SpaceTestContext>((context) => {
+    context.server.stop();
+  });
+
+  describe('get', () => {
+    it<SpaceTestContext>('creates a space with the correct name', ({ client }) => {
+      const channels = client.channels;
+      const channelSpy = vi.spyOn(channels, 'get');
+      const space = new Space('test', client);
+
+      expect(channelSpy).toHaveBeenNthCalledWith(1, '_ably_space_test');
+      // Note: This is matching the class type. This is not a TypeScript type.
+      expectTypeOf(space).toMatchTypeOf<Space>();
     });
   });
 
-  it<SpaceTestContext>('Creates a space successfully with call to channel.get', ({ client }) => {
-    const channels = client.channels;
-    const channelSpy = vi.spyOn(channels, 'get');
-    const space = new Space('test', client);
-    expect(channelSpy).toHaveBeenCalledOnce();
-    expect(channelSpy).toHaveBeenCalledWith('_ably_space_test');
-    // Note: This is matching the class type. This is not a TypeScript type.
-    expectTypeOf(space).toMatchTypeOf<Space>();
-  });
+  describe('enter', () => {
+    it<SpaceTestContext>('enters a space successfully', async ({ client, server }) => {
+      const spaceName = '_ably_space_test';
+      const actionsOverride = {
+        channel: spaceName,
+      };
 
-  it('Enters the space successfully', async () => {
-    // const server = createMockServer();
-    // waitForFirstMessage(server);
-    const server = new Server();
-    server.connect()
-    server.addAction()
-    // console.log(server.actions)
+      server.onAction(10, createChannelAction(actionsOverride));
+      server.onAction(10, getPresenceAction(actionsOverride));
+      server.onActionCallback(14, (msg) => {
+        const data = JSON.stringify(msg.presence[0].data);
 
-    // server.on('connection', (socket) => {
-    //   socket.send(
-    //     JSON.stringify({
-    //       action: 4,
-    //       connectionId: 'CONNDESC',
-    //       connectionKey: 'CONNECTIONKEY',
-    //       connectionSerial: -1,
-    //       connectionDetails: {
-    //         clientId: 'RND-CLIENTID',
-    //         connectionKey: 'randomKey',
-    //         maxMessageSize: 131000,
-    //         maxInboundRate: 1000,
-    //         maxOutboundRate: 1000,
-    //         maxFrameSize: 262144,
-    //         connectionStateTtl: 120000,
-    //         maxIdleInterval: 15000,
-    //       },
-    //     })
-    //   );
-
-
-    //   socket.on('message', (m) => {
-    //     try {
-    //       const obj = JSON.parse(m as string);
-    //       if (obj.action === 10) {
-    //         socket.send(
-    //           JSON.stringify({
-    //             action: 11,
-    //             flags: 983041,
-    //             channel: '_ably_space_test',
-    //             channelSerial: '108eMNtswBL6Ud51959078:-1',
-    //           })
-    //         );
-    //         socket.send(
-    //           JSON.stringify({
-    //             action: 16,
-    //             channel: '_ably_space_test',
-    //             channelSerial: '108rUpQvABKxWa69183736:',
-    //             presence: [],
-    //           })
-    //         );
-    //       }
-    //       if (obj.action === 14) {
-    //         const data = JSON.stringify(obj.presence[0].data);
-    //         socket.send(
-    //           JSON.stringify({
-    //             action: 14,
-    //             id: 'NU_ExvNktu:0',
-    //             connectionId: 'NU_ExvNktu',
-    //             connectionSerial: 0,
-    //             channel: '_ably_space_test',
-    //             channelSerial: '108eMNtswBL6Ud51959078:3',
-    //             timestamp: 1675699722722,
-    //             presence: [
-    //               {
-    //                 id: 'NU_ExvNktu:0:0',
-    //                 clientId: 'T7we24YJhMvTDrxQC1pvH',
-    //                 connectionId: 'NU_ExvNktu',
-    //                 timestamp: 1675699722722,
-    //                 encoding: 'json',
-    //                 data: data,
-    //                 action: 2,
-    //               },
-    //             ],
-    //           })
-    //         );
-    //       }
-    //     } catch (e) {
-    //       console.error(e);
-    //     }
-    //   });
-    // });
-
-    const client = new Ably.Realtime({
-      key: 'abc:def',
-      useBinaryProtocol: false,
-      log: {
-        level: 4,
-        handler: console.log,
-      },
-    });
-    const space = new Space('test', client);
-    space.enter({ name: 'John' });
-
-    const channel = client.channels.get('_ably_space_test');
-
-    let data: { name: string } = await new Promise((fulfill) => {
-      channel.presence.subscribe('enter', (d) => {
-        fulfill(JSON.parse(d.data));
+        return enterPresenceAction({
+          ...actionsOverride,
+          presence: [
+            {
+              ...enterPresenceAction({}).presence[0],
+              data,
+            },
+          ],
+        });
       });
+
+      server.start();
+
+      const spaceData = { name: 'John' };
+      const space = new Space('test', client);
+      space.enter(spaceData);
+
+      const channel = client.channels.get(spaceName);
+
+      const data: { name: string } = await new Promise((fulfill) => {
+        channel.presence.subscribe('enter', (d) => fulfill(JSON.parse(d.data)));
+      });
+
+      expect(data).toEqual(spaceData);
     });
-    expect(data.name).to.equal('John');
   });
 });
--- src/Spaces.test.ts
+++ src/Spaces.test.ts
@@ -1,63 +1,46 @@
-import { it, describe, expect, expectTypeOf, vi } from 'vitest';
+import { it, describe, expect, expectTypeOf, vi, beforeEach, afterEach } from 'vitest';
 import Ably, { Types } from 'ably/promises';
+import { WebSocket } from 'mock-socket';
+
 import Space from './Space';
 import Spaces from './Spaces';
-import { Server, WebSocket } from 'mock-socket';
 
-describe('Core Space API functionality', () => {
-  it('Expects the injected client to be of the type RealtimePromise', () => {
-    const ablyClient = new Ably.Realtime({
-      key: 'abc:def',
-    });
-    const spaces = new Spaces(ablyClient);
+import Server from '../utilities/test/mock-server';
+import defaultClientConfig from '../utilities/test/default-client-config';
+
+interface SpacesTestContext {
+  client: Types.RealtimePromise;
+  server: Server;
+}
+
+describe('Spaces', () => {
+  beforeEach<SpacesTestContext>((context) => {
+    (Ably.Realtime as any).Platform.Config.WebSocket = WebSocket;
+    context.server = new Server('wss://realtime.ably.io/');
+    context.client = new Ably.Realtime(defaultClientConfig);
+  });
+
+  afterEach<SpacesTestContext>((context) => {
+    context.server.stop();
+  });
+
+  it<SpacesTestContext>('expects the injected client to be of the type RealtimePromise', ({ client }) => {
+    const spaces = new Spaces(client);
     expectTypeOf(spaces.ably).toMatchTypeOf<Types.RealtimePromise>();
   });
-  it('Connects successfully with the Ably Client', async () => {
-    (Ably.Realtime as any).Platform.Config.WebSocket = WebSocket;
-    const server = new Server('wss://realtime.ably.io/');
-    server.on('connection', (socket) => {
-      socket.send(
-        JSON.stringify({
-          action: 4,
-          connectionId: 'CONNDESC',
-          connectionKey: 'CONNECTIONKEY',
-          connectionSerial: -1,
-          connectionDetails: {
-            clientId: 'RND-CLIENTID',
-            connectionKey: 'randomKey',
-            maxMessageSize: 131000,
-            maxInboundRate: 1000,
-            maxOutboundRate: 1000,
-            maxFrameSize: 262144,
-            connectionStateTtl: 120000,
-            maxIdleInterval: 15000,
-          },
-        })
-      );
-      socket.on('message', (m) => {
-        console.log(m);
-      });
-    });
-    const ablyClient = new Ably.Realtime({
-      key: 'abc:def',
-      useBinaryProtocol: false,
-      log: {
-        level: 4,
-        handler: console.log,
-      },
-    });
-    const connectSuccess = await ablyClient.connection.whenState('connected');
+
+  it<SpacesTestContext>('connects successfully with the Ably Client', async ({ client, server }) => {
+    server.start();
+    const connectSuccess = await client.connection.whenState('connected');
     expect(connectSuccess.current).toBe('connected');
   });
 
-  it('Creates and retrieves spaces successfully', () => {
-    const ablyClient = new Ably.Realtime({
-      key: 'abc:def',
-    });
-    const channels = ablyClient.channels;
+  it<SpacesTestContext>('creates and retrieves spaces successfully', ({ client }) => {
+    const channels = client.channels;
     const spy = vi.spyOn(channels, 'get');
-    const spaces = new Spaces(ablyClient);
+    const spaces = new Spaces(client);
     const space = spaces.get('test');
+
     expect(spy).toHaveBeenCalledOnce();
     expect(spy).toHaveBeenCalledWith('_ably_space_test');
     // Note: This is matching the class type. This is not a TypeScript type.
