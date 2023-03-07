7e083fd88ce275169669636ad5d6774b13aee000
Peter Maguire <peter.maguire@ably.com>
2023-03-07 13:17:11 +0000

    Add ably-agent header for usage tracking (#16)
    Adds ably-agent as outlined in DR2

--- src/Spaces.test.ts
+++ src/Spaces.test.ts
@@ -46,4 +46,36 @@ describe('Spaces', () => {
     // Note: This is matching the class type. This is not a TypeScript type.
     expectTypeOf(space).toMatchTypeOf<Space>();
   });
+
+  it<SpacesTestContext>('creates a client with default options when a key is passed in', () => {
+    const spaces = new Spaces(defaultClientConfig.key);
+    expect(spaces.ably['options'].key).toEqual(defaultClientConfig.key);
+  });
+
+  it<SpacesTestContext>('creates a client with options that are passed in', () => {
+    const spaces = new Spaces({ ...defaultClientConfig });
+    expect(spaces.ably['options']).toContain(defaultClientConfig);
+  });
+
+  it<SpacesTestContext>('applies the agent header to an existing SDK instance', ({ client }) => {
+    const spaces = new Spaces(client);
+    expect((client as any).options.agents).toEqual([`ably-spaces/${spaces.version}`, 'space-custom-client']);
+  });
+
+  it<SpacesTestContext>('applies the agent header when options are passed in', () => {
+    const spaces = new Spaces(defaultClientConfig);
+    expect(spaces.ably['options'].agents).toEqual([`ably-spaces/${spaces.version}`, 'space-default-client']);
+  });
+
+  it<SpacesTestContext>('extend the agents array when it already exists', () => {
+    const spaces = new Spaces({
+      ...defaultClientConfig,
+      agents: ['some-client/1.2.3'],
+    } as any);
+    expect(spaces.ably['options'].agents).toEqual([
+      'some-client/1.2.3',
+      `ably-spaces/${spaces.version}`,
+      'space-default-client',
+    ]);
+  });
 });
--- src/Spaces.ts
+++ src/Spaces.ts
@@ -1,15 +1,34 @@
-import { Types } from 'ably';
+import { Types, Realtime } from 'ably';
 import SpaceOptions from './options/SpaceOptions';
 import Space from './Space';
-
 class Spaces {
   private spaces: Record<string, Space>;
   private channel: Types.RealtimeChannelPromise;
   ably: Types.RealtimePromise;
 
-  constructor(ably: Types.RealtimePromise) {
+  readonly version = '0.0.1';
+
+  constructor(optionsOrAbly: Types.RealtimePromise | Types.ClientOptions | string) {
     this.spaces = {};
-    this.ably = ably;
+    if (optionsOrAbly instanceof Realtime) {
+      this.ably = optionsOrAbly as Types.RealtimePromise;
+      this.addAgent(this.ably['options'], false);
+    } else {
+      let options: Types.ClientOptions = typeof optionsOrAbly === 'string' ? { key: optionsOrAbly } : optionsOrAbly;
+      this.addAgent(options, true);
+      this.ably = new Realtime.Promise(options);
+    }
+    this.ably.time();
+  }
+
+  private addAgent(options: any, isDefault: boolean) {
+    const agent = `ably-spaces/${this.version}`;
+    const clientType = isDefault ? 'space-default-client' : 'space-custom-client';
+    if (!options.agents) {
+      options.agents = [agent, clientType];
+    } else if (!options.agents.includes(agent)) {
+      options.agents.push(agent, clientType);
+    }
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
