2a5b3f4d0de0a65cb4fa09f9cd5b7b5d834f9085
Peter Maguire <peter.maguire@ably.com>
2023-03-30 13:28:46 +0100

    Linting/formatting

--- src/Locations.mockClient.test.ts
+++ src/Locations.mockClient.test.ts
@@ -1,6 +1,6 @@
 import { it, describe, expect, vi, beforeEach } from 'vitest';
 import { Realtime, Types } from 'ably/promises';
-import Space, { SpaceMember } from "./Space";
+import Space, { SpaceMember } from './Space';
 import { createPresenceMessage } from './utilities/test/fakes';
 
 interface SpaceTestContext {
@@ -10,8 +10,6 @@ interface SpaceTestContext {
 }
 
 vi.mock('ably/promises');
-
-
 
 describe('Locations (mockClient)', () => {
   beforeEach<SpaceTestContext>((context) => {
@@ -26,7 +24,6 @@ describe('Locations (mockClient)', () => {
     context.client = client;
     context.space = new Space('test', client);
     context.presence = presence;
-
   });
 
   describe('set', () => {
@@ -34,39 +31,45 @@ describe('Locations (mockClient)', () => {
       expect(() => space.locations.set('location1')).toThrowError();
     });
 
-    it<SpaceTestContext>('sends a presence update on location set', async ({ client, space, presence }) => {
+    it<SpaceTestContext>('sends a presence update on location set', async ({ space, presence }) => {
       const spy = vi.spyOn(presence, 'update');
       await space.enter();
       space.locations.set('location1');
       expect(spy).toHaveBeenCalledOnce();
     });
 
-    it<SpaceTestContext>('fires an event when a location is set', async ({ client, space }) => {
+    it<SpaceTestContext>('fires an event when a location is set', async ({ space }) => {
       const spy = vi.fn();
       await space.enter();
-      space.locations.on("locationUpdate", spy);
-      space.locations["onPresenceUpdate"](createPresenceMessage('update', { clientId: '2', connectionId: '2', data: { location: "location2" } }))
+      space.locations.on('locationUpdate', spy);
+      space.locations['onPresenceUpdate'](
+        createPresenceMessage('update', { clientId: '2', connectionId: '2', data: { location: 'location2' } }),
+      );
       expect(spy).toHaveBeenCalledOnce();
     });
 
-    it<SpaceTestContext>('correctly sets previousLocation', async ({ client, space, presence }) => {
-      const spy =  vi.fn();
+    it<SpaceTestContext>('correctly sets previousLocation', async ({ space }) => {
+      const spy = vi.fn();
       await space.enter();
-      space.locations.on("locationUpdate", spy);
-      space.locations["onPresenceUpdate"](createPresenceMessage('update', { clientId: '2', connectionId: '2', data: { location: "location1" } }))
-      space.locations["onPresenceUpdate"](createPresenceMessage('update', { clientId: '2', connectionId: '2', data: { location: "location2" } }))
-      expect(spy).toHaveBeenLastCalledWith<{member: SpaceMember, currentLocation: any, previousLocation: any}[]>({
+      space.locations.on('locationUpdate', spy);
+      space.locations['onPresenceUpdate'](
+        createPresenceMessage('update', { clientId: '2', connectionId: '2', data: { location: 'location1' } }),
+      );
+      space.locations['onPresenceUpdate'](
+        createPresenceMessage('update', { clientId: '2', connectionId: '2', data: { location: 'location2' } }),
+      );
+      expect(spy).toHaveBeenLastCalledWith<{ member: SpaceMember; currentLocation: any; previousLocation: any }[]>({
         member: {
           clientId: '2',
           isConnected: true,
           profileData: { a: 1 },
           location: 'location2',
           lastEvent: { name: 'update', timestamp: 1 },
-          connections: [ '2' ]
+          connections: ['2'],
         },
         currentLocation: 'location2',
-        previousLocation: 'location1'
-      })
+        previousLocation: 'location1',
+      });
     });
   });
 });
