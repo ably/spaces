ddf653cb3cdfdcd8a1df3de1a119bf34977bdc46
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2023-04-20 10:29:17 +0100

    Position avatar stack correctly

--- demo/app/location-tracking/track-slide-elements.ts
+++ demo/app/location-tracking/track-slide-elements.ts
@@ -3,7 +3,7 @@ import { createFragment, queryDataId } from '../utils/dom';
 import { nameToInitials } from '../utils/fake-names';
 import { gradients } from '../utils/gradients';
 
-const slideSelectedClasses = ['mb-[200px]'];
+const slideSelectedClasses = ['mb-[140px]'];
 
 const renderPreviewAvatar = (member: SpaceMember, index: number) => {
   const fragment = createFragment('#avatar-template');
@@ -14,10 +14,10 @@ const renderPreviewAvatar = (member: SpaceMember, index: number) => {
     return;
   }
 
+  queryDataId(fragment, 'avatar-hover').remove();
+
   const initials = queryDataId(fragment, 'name');
   initials.innerHTML = nameToInitials(member.profileData.name);
-  const fullNameEl = queryDataId(fragment, 'avatar-full-name');
-  fullNameEl.innerHTML = member.profileData.name;
 
   const innerWrapper = queryDataId(fragment, 'avatar-inner-wrapper');
 
@@ -34,7 +34,7 @@ const renderPreviewAvatar = (member: SpaceMember, index: number) => {
 
 export const renderPreviewStack = (slideMembers: SpaceMember[], slideId) => {
   const ul = document.createElement('ul');
-  ul.classList.add('flex');
+  ul.classList.add('flex', 'absolute', 'scale-[3]', 'top-[800px]', 'left-[1220px]', 'translate-x-[-100%]');
   ul.setAttribute('id', `preview-stack-${slideId}`);
 
   slideMembers.forEach((member, index) => {
@@ -49,28 +49,33 @@ export const renderPreviewStack = (slideMembers: SpaceMember[], slideId) => {
   return ul;
 };
 
+const updatePreviewAvatarStack = (space: Space, htmlElement: HTMLElement, selfId: string, slideId: string) => {
+  const members = space.getMembers();
+  const slideMembers = members.filter(
+    (member) => member.location && member.location.startsWith(slideId) && member.clientId !== selfId,
+  );
+
+  if (slideMembers.length > 0) {
+    htmlElement.classList.add(...slideSelectedClasses);
+  }
+
+  const previewStack = renderPreviewStack(slideMembers, slideId);
+
+  document.getElementById(`preview-stack-${slideId}`)?.remove();
+  htmlElement.appendChild(previewStack);
+};
+
 const selectSlideElement =
   (space: Space, slideId: string) =>
-  (htmlElement: HTMLElement, userName: string, newClientId: string, selfId: string, memberIndex: number) => {
-    const members = space.getMembers();
-    const slideMembers = members.filter(
-      (member) => member.location && member.location.startsWith(slideId) && member.clientId !== selfId,
-    );
-
-    if (slideMembers.length > 0) {
-      htmlElement.classList.add(...slideSelectedClasses);
-    }
-
-    const previewStack = renderPreviewStack(slideMembers, slideId);
-
-    document.getElementById(`preview-stack-${slideId}`)?.remove();
-    htmlElement.appendChild(previewStack);
+  (htmlElement: HTMLElement, userName: string, newClientId: string, selfId: string) => {
+    updatePreviewAvatarStack(space, htmlElement, selfId, slideId);
   };
 
 const deselectSlideElement =
   (space: Space, slideId: string) =>
-  (htmlElement: HTMLElement, userName: string, oldClientId: string, selfId: string, memberIndex: number) => {
+  (htmlElement: HTMLElement, userName: string, oldClientId: string, selfId: string) => {
     htmlElement.classList.remove(...slideSelectedClasses);
+    updatePreviewAvatarStack(space, htmlElement, selfId, slideId);
   };
 
 export const createSlideElementManager = (space: Space, slideId: string) => ({
