efa2205de8a7824b469c4215242dddd2e0cc21c5
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2023-04-21 08:53:27 +0100

    Add self avatar to preview avatar stack

--- demo/app/location-tracking/track-slides.ts
+++ demo/app/location-tracking/track-slides.ts
@@ -5,7 +5,7 @@ import { gradients } from '../utils/gradients';
 
 const slideSelectedClasses = ['mb-[140px]'];
 
-const renderPreviewAvatar = (member: SpaceMember, index: number) => {
+const renderPreviewAvatar = (member: SpaceMember, index: number, isSelf?: boolean) => {
   const fragment = createFragment('#avatar-template');
 
   const memberName = member.profileData?.name;
@@ -21,30 +21,48 @@ const renderPreviewAvatar = (member: SpaceMember, index: number) => {
 
   const innerWrapper = queryDataId(fragment, 'avatar-inner-wrapper');
 
+  const [fromColor, toColor] = [
+    isSelf ? 'from-blue-400' : gradients[index][0],
+    isSelf ? 'to-blue-500' : gradients[index][1],
+  ];
+
   if (member.isConnected) {
-    innerWrapper.classList.add('bg-gradient-to-b', gradients[index][0], gradients[index][1]);
+    innerWrapper.classList.add('bg-gradient-to-b', fromColor, toColor);
     innerWrapper.classList.remove('bg-[##D0D3DC]');
   } else {
     innerWrapper.classList.add('bg-[#D0D3DC]');
-    innerWrapper.classList.remove('bg-gradient-to-b', gradients[index][0], gradients[index][1]);
+    innerWrapper.classList.remove('bg-gradient-to-b', fromColor, toColor);
   }
 
   return fragment;
 };
 
-const renderPreviewStack = (slideMembers: { member: SpaceMember; i: number }[], slideId: string) => {
+const addAvatarToList = (ul: HTMLUListElement, avatar?: Node) => {
+  if (avatar) {
+    const li = document.createElement('li');
+    li.classList.add('ml-[-9px]', 'relative');
+    li.appendChild(avatar);
+    ul.appendChild(li);
+  }
+};
+
+const renderPreviewStack = (
+  slideMembers: { member: SpaceMember; i: number }[],
+  self: SpaceMember | false,
+  slideId: string,
+) => {
   const ul = document.createElement('ul');
   ul.classList.add('flex', 'absolute', 'scale-[3]', 'top-[800px]', 'left-[1220px]', 'translate-x-[-100%]');
   ul.setAttribute('id', `preview-stack-${slideId}`);
 
+  if (self) {
+    const avatar = renderPreviewAvatar(self, 0, true);
+    addAvatarToList(ul, avatar);
+  }
+
   slideMembers.forEach(({ member, i }) => {
     const avatar = renderPreviewAvatar(member, i);
-    if (avatar) {
-      const li = document.createElement('li');
-      li.classList.add('ml-[-9px]', 'relative');
-      li.appendChild(avatar);
-      ul.appendChild(li);
-    }
+    addAvatarToList(ul, avatar);
   });
   return ul;
 };
@@ -56,11 +74,14 @@ const updatePreviewAvatarStack = (space: Space, htmlElement: HTMLElement, selfId
     .map((member, i) => ({ member, i }))
     .filter(({ member }) => member.location && member.location.startsWith(slideId));
 
-  if (slideMembers.length > 0) {
+  const self = space.getSelf();
+  const selfIsPresent = self.location && self.location.startsWith(slideId);
+
+  if (slideMembers.length > 0 || selfIsPresent) {
     htmlElement.classList.add(...slideSelectedClasses);
   }
 
-  const previewStack = renderPreviewStack(slideMembers, slideId);
+  const previewStack = renderPreviewStack(slideMembers, selfIsPresent && self, slideId);
 
   document.getElementById(`preview-stack-${slideId}`)?.remove();
   htmlElement.appendChild(previewStack);
