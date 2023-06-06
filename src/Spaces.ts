8fe1384063f8b79f5d48e395d482bcd75a73c15d
Dominik Piatek <dominik.piatek@ably.io>
2023-06-06 15:46:45 +0100

    Bump to 0.0.7

--- src/Spaces.ts
+++ src/Spaces.ts
@@ -7,7 +7,7 @@ class Spaces {
   private channel: Types.RealtimeChannelPromise;
   ably: Types.RealtimePromise;
 
-  readonly version = '0.0.6';
+  readonly version = '0.0.7';
 
   constructor(optionsOrAbly: Types.RealtimePromise | Types.ClientOptions | string) {
     this.spaces = {};
