c94f41ae3300fcfc35fb50bc0906047045012165
Arti M <arti.mathanda@ably.com>
2023-06-30 14:47:56 +0100

    chore: Makes top nav responsive

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   content: ['./app/index.html', './app/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens: {
+        xs: '375px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
       colors: {
         'ably-black': '#03020D',
         'ably-charcoal-grey': '#292831',
