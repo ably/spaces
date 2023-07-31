27b09f0131898a25b169c5d14d15d037aa5e155f
Dominik Piatek <dominik.piatek@ably.com>
2023-07-31 11:28:38 +0100

    Set initial state for members in hooks

--- demo-react/src/hooks/useMembers.ts
+++ demo-react/src/hooks/useMembers.ts
@@ -2,6 +2,9 @@ import { useEffect, useState, useContext } from 'react';
 import { type SpaceMember } from '@ably-labs/spaces';
 import { SpacesContext } from '../components';
 
+const membersToOthers = (members: SpaceMember[] = [], self: SpaceMember | undefined): SpaceMember[] =>
+  members.filter((m) => m.connectionId !== self?.connectionId);
+
 export const useMembers: () => Partial<{ self: SpaceMember; others: SpaceMember[]; members: SpaceMember[] }> = () => {
   const space = useContext(SpacesContext);
   const [members, setMembers] = useState<SpaceMember[]>([]);
@@ -10,6 +13,13 @@ export const useMembers: () => Partial<{ self: SpaceMember; others: SpaceMember[
 
   useEffect(() => {
     if (!space) return;
+
+    const initSelf = space.getSelf();
+    const initMembers = space.getMembers();
+
+    setSelf(initSelf);
+    setMembers(initMembers);
+    setOthers(membersToOthers(initMembers, initSelf));
 
     const handler = (members: SpaceMember[]) => {
       const self = space.getSelf();
