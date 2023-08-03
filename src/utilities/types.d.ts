3f6a095b7bdcbe37e1af10663f3bba227e5375fb
Dominik Piatek <dominik.piatek@ably.com>
2023-08-03 14:21:25 +0100

    Add types files
    These are split into an external and internal ones, the latter being ones that we might want to change without introducing breaking changes.

---
+++ src/utilities/types.d.ts
@@ -0,0 +1,34 @@
+import { Types } from 'ably';
+
+import { EventKey, EventListener, EventMap } from './EventEmitter.js';
+import { ProfileData } from '../types.js';
+
+export type PresenceMember = {
+  data: {
+    profileUpdate: {
+      id: string | null;
+      current: ProfileData;
+    };
+    locationUpdate: {
+      id: string | null;
+      previous: unknown;
+      current: unknown;
+    };
+  };
+} & Omit<Types.PresenceMessage, 'data'>;
+
+export type Subset<K> = {
+  [attr in keyof K]?: K[attr] extends object ? Subset<K[attr]> : K[attr];
+};
+
+export interface Provider<ProviderEventMap extends EventMap> {
+  subscribe<K extends EventKey<ProviderEventMap>>(
+    listenerOrEvents?: K | K[] | EventListener<ProviderEventMap[K]>,
+    listener?: EventListener<ProviderEventMap[K]>,
+  );
+
+  unsubscribe<K extends EventKey<ProviderEventMap>>(
+    listenerOrEvents?: K | K[] | EventListener<ProviderEventMap[K]>,
+    listener?: EventListener<ProviderEventMap[K]>,
+  );
+}
