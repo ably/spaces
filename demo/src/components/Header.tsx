6050695fbcd68d9ad020e50215d78d96d424df5d
Dominik Piatek <dominik.piatek@ably.com>
2023-08-10 12:05:54 +0100

    CSS demo fixes

--- demo/src/components/Header.tsx
+++ demo/src/components/Header.tsx
@@ -14,22 +14,23 @@ export const Header = ({ self, others }: Props) => {
       id="main-header"
       className="bg-white"
     >
-      <div className="mx-auto justify-between grid grid-rows-2 grid-cols-2 md:max-w-screen-2xl md:px-16 md:flex md:items-center">
-        <section className="py-4">
+      <div className="mx-auto justify-between grid grid-rows-2 grid-cols-2 max-w-screen-2xl md:px-8 lg:px-16 md:flex md:items-center">
+        <section className="py-4 shrink-0 mr-4">
           <p className="font-semibold pl-8 md:text-2xl">Team Argo</p>
           <p className="leading-5 pl-8">Pitch deck</p>
         </section>
 
-        <section className="ml-auto flex group relative pr-8 py-4">
-          <InfoSvg className="text-ably-black" />
-          <p className="ml-2 xs:text-sm md:font-medium md:text-base">How to try this demo</p>
-          <div className="group-hover p-4 bg-[#FAFAFB] rounded-lg hidden group-hover:block absolute top-full mt-2 w-[300px] -left-[70%] border border-[#D9D9DA] text-xs md:text-sm z-20">
+        <section className="ml-auto group relative pr-8 py-4 group-hover cursor-pointer flex">
+          <InfoSvg className="text-ably-black self-center shrink-0" />
+          <p className="ml-2 font-medium text-sm lg:text-base self-center">How to try this demo</p>
+          <div className="p-4 bg-[#FAFAFB] rounded-lg hidden group-hover:block absolute top-[50px] mt-2 w-[300px] -left-[100px] md:-left-[50px] border border-[#D9D9DA] text-sm z-20">
             Open this page in multiple windows or share the URL with your team to try out the live avatar stack.
           </div>
         </section>
+
         <section
           id="avatar-stack-container"
-          className="flex justify-end col-span-2 xs:border-t xs:border-[#D9D9DA] xs:ml-0 xs:pr-8 md:ml-8 md:border-t-0 py-4"
+          className="flex justify-end items-baseline col-span-2 border-t border-[#D9D9DA] md:border-t-0 p-4"
         >
           <>
             {self && (
@@ -42,11 +43,11 @@ export const Header = ({ self, others }: Props) => {
           </>
         </section>
 
-        <section className="xs:hidden md:ml-[24px] md:flex md:items-center">
+        <section className="hidden md:flex md:items-center">
           <a
             href="https://github.com/ably-labs/spaces"
             target="_blank"
-            className="flex items-center px-5 py-[14px] justify-start"
+            className="flex items-center px-5 py-[14px] justify-start shrink-0 block"
             rel="noreferrer"
           >
             <p className="font-medium text-base">Space API</p>
@@ -55,7 +56,7 @@ export const Header = ({ self, others }: Props) => {
 
           <a
             href="https://docs.google.com/forms/d/e/1FAIpQLSer2ujLNw0rlrf2FvfIhLxyiWuuvTwYkDDqHmv30F8Cs00YWg/viewform"
-            className="text-white bg-ably-black rounded-md py-[11px] px-5 leading-[1.125] md:text-xs lg:text-base inline-block lg:ml-[24px]"
+            className="block w-[100px] text-white bg-ably-black rounded-md py-[11px] px-5 leading-[1.125] md:text-xs lg:text-base lg:ml-[24px] shrink-0 hidden lg:block"
           >
             Sign Up
           </a>
