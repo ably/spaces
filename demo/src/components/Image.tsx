1e4fe9ad44f7e6ffd7b084795c897666e17d4033
evgeny <khokhlov.e.n@gmail.com>
2023-08-29 21:58:50 +0100

    fix: locking demo bugs
    * The 'click elsewhere to lose selection' applied to locations as well, not just locks
    * When more than one person is on the same component (the pictures that are not locked), the location label say "... +N".
    * When someone leaves, the avatar change to grey
    * Sanitize value from `useChannel`

--- demo/src/components/Image.tsx
+++ demo/src/components/Image.tsx
@@ -1,6 +1,8 @@
 import cn from 'classnames';
-import { useElementSelect, useMembers } from '../hooks';
-import { findActiveMember, getMemberFirstName, getOutlineClasses } from '../utils';
+import { useClickOutside, useElementSelect, useMembers } from '../hooks';
+import { findActiveMembers, getMemberFirstName, getOutlineClasses } from '../utils';
+import { useRef } from 'react';
+import { usePreview } from './PreviewContext.tsx';
 
 interface Props extends React.HTMLAttributes<HTMLImageElement> {
   src: string;
@@ -12,19 +14,26 @@ interface Props extends React.HTMLAttributes<HTMLImageElement> {
 }
 
 export const Image = ({ src, children, className, id, slide, locatable = true }: Props) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const preview = usePreview();
   const { members, self } = useMembers();
   const { handleSelect } = useElementSelect(id, false);
-  const activeMember = findActiveMember(id, slide, members);
-  const { outlineClasses, stickyLabelClasses } = getOutlineClasses(activeMember);
-  const memberName = getMemberFirstName(activeMember);
-  const label = self?.connectionId === activeMember?.connectionId ? 'You' : memberName;
+  const activeMembers = findActiveMembers(id, slide, members);
+  const locatedByMe = activeMembers.some((member) => member.connectionId === self?.connectionId);
+  const [firstMember] = activeMembers;
+  const { outlineClasses, stickyLabelClasses } = getOutlineClasses(firstMember);
+  const memberName = getMemberFirstName(firstMember);
+  const name = locatedByMe ? 'You' : memberName;
+  const label = activeMembers.length > 1 ? `${name} +${activeMembers.length - 1}` : name;
+
+  useClickOutside(containerRef, self, locatedByMe && !preview);
 
   return (
     <div
       data-before={label}
       className={cn('relative xs:my-4 md:my-0', className, {
         [`outline-2 outline before:content-[attr(data-before)] before:absolute before:-top-[22px] before:-left-[2px] before:px-[10px] before:text-sm before:text-white before:rounded-t-lg before:normal-case ${outlineClasses} before:${stickyLabelClasses}`]:
-          activeMember,
+          firstMember,
       })}
     >
       <img
