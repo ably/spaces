2e27f081a8f366f953ffb310d0b1832f07c998e1
Nikita Kakuev <n.kakuev@blexr.com>
2023-09-21 22:56:03 +0100

    Small refactor

--- demo/src/utils/url.ts
+++ demo/src/utils/url.ts
@@ -1,13 +1,13 @@
 import { generate } from 'random-words';
 
-const getParamNameFromUrl = (paramName: string = 'space', length = 3, join = '-' ) => {
+const getParamNameFromUrl = (paramName = 'space', exactly = 3, join = '-' ) => {
   const url = new URL(window.location.href);
   const spaceNameInParams = url.searchParams.get(paramName);
 
   if (spaceNameInParams) {
     return spaceNameInParams;
   } else {
-    const generatedName = generate({ exactly: length, join });
+    const generatedName = generate({ exactly, join });
     url.searchParams.set(paramName, generatedName);
     window.history.replaceState({}, '', `?${url.searchParams.toString()}`);
     return generatedName;
