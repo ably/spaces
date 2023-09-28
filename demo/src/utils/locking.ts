feb5688a4db58c362d2ed6c8576b4b172a30f455
Dominik Piatek <dominik.piatek@ably.com>
2023-09-28 12:21:07 +0100

    Fix locks release method

--- demo/src/utils/locking.ts
+++ demo/src/utils/locking.ts
@@ -1,7 +1,11 @@
 import { Space } from '@ably/spaces';
 
 export const releaseMyLocks = async (space: Space) => {
-  await Promise.all([...space.locks.getSelf().map((lock) => space.locks.release(lock.id))]);
+  const locks = await space.locks.getSelf();
+
+  if (locks.length > 0) {
+    locks.forEach((lock) => space.locks.release(lock.id));
+  }
 };
 
 export const buildLockId = (slide: string | undefined, element: string | undefined) =>
