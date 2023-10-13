55f7e3b9daf9c843c13e56b12703ceff95aa3f7d
evgeny <khokhlov.e.n@gmail.com>
2023-10-13 20:09:35 +0100

    fix: add missed exports

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,7 +1,9 @@
 export { SpacesProvider } from './contexts/SpacesContext.js';
 export { SpaceProvider } from './contexts/SpaceContext.js';
 export { useSpaces } from './useSpaces.js';
+export { useSpace } from './useSpace.js';
 export { useMembers } from './useMembers.js';
 export { useLocations } from './useLocations.js';
 export { useLocks } from './useLocks.js';
 export { useLock } from './useLock.js';
+export { useCursors } from './useCursors.js';
