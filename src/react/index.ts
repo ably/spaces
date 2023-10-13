aefc89bb75ed14404bbed7c1d3ee59f5211a83dd
evgeny <khokhlov.e.n@gmail.com>
2023-10-13 11:14:47 +0100

    feat: add exports

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,2 +1,7 @@
 export { SpacesProvider } from './contexts/SpacesContext.js';
+export { SpaceProvider } from './contexts/SpaceContext.js';
 export { useSpaces } from './useSpaces.js';
+export { useMembers } from './useMembers.js';
+export { useLocations } from './useLocations.js';
+export { useLocks } from './useLocks.js';
+export { useLock } from './useLock.js';
